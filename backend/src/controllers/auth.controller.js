81c354985ab840adaea60c60645c17bbb2696f9b
shaurya super <shauryabansal2005@gmail.com>
2025-06-04 11:46:32 +0530

    added login and logout end points and integrated stream

--- backend/src/controllers/auth.controller.js
+++ backend/src/controllers/auth.controller.js
@@ -1,5 +1,6 @@
 import User from "../models/User.js";
 import jwt from "jsonwebtoken";
+import { upsertStreamUser } from "../lib/stream.js";
 
 export async function signup(req, res) {
     const {email,password,fullName} = req.body;
@@ -35,6 +36,20 @@ export async function signup(req, res) {
             profilePic: randomAvatar
         });
 
+        try {
+            await upsertStreamUser({
+            id: newUser._id.toString(),
+            name: newUser.fullName,
+            image: newUser.profilePic || ""
+        });
+
+        console.log(`Stream user created for new user: ${newUser.fullName}`);
+
+        } catch (error) {
+            console.error("Error creating stream user:", error);
+        }
+
+
         const token = jwt.sign({userId:newUser._id},process.env.JWT_SECRET_KEY , {
             expiresIn: "7d"
         })
@@ -54,9 +69,41 @@ export async function signup(req, res) {
 }
 
 export async function login(req, res) {
-    res.send("Login Route");
+    try {
+        const {email,password} = req.body;
+        if(!email || !password){
+            res.status(400).json({message: "All fields are required"});
+        }
+
+        const user = await User.findOne({email}) ;
+        if(!user){
+            return res.status(401).json({message: "Invalid email or password"});
+        }
+        const isPasswordCorrect = await user.matchPassword(password);
+        if(!isPasswordCorrect){
+            return res.status(401).json({message: "Invalid email or password"});
+        }
+
+        const token = jwt.sign({userId:user._id},process.env.JWT_SECRET_KEY , {
+            expiresIn: "7d"
+        })
+        res.cookie("jwt" , token , {
+            maxAge: 7 * 24 * 60 * 60 * 1000,
+            httpOnly: true,
+            sameSite: "Strict",
+            secure: process.env.NODE_ENV === "production" 
+        })
+
+        res.status(200).json({success: true, user});
+        
+    } catch (error) {
+        console.log("Error in login controller", error);
+        res.status(500).json({message: "Internal server error"});
+        
+    }
 }
 
 export function logout(req, res) {
-    res.send("Logout Route");
+    res.clearCookie("jwt");
+    res.status(200).json({succeess: true , message: "Logged out successfully"});
 }
