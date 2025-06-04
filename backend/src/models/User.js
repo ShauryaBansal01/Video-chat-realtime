ff90d502053360a8d7d3074b9ac38322cd03b24c
shaurya super <shauryabansal2005@gmail.com>
2025-06-04 09:11:45 +0530

    signup endpoint

--- backend/src/models/User.js
+++ backend/src/models/User.js
@@ -46,20 +46,23 @@ const userSchema = new mongoose.Schema({
     }]
 },{timestamps: true});
 
-const User = mongoose.model("User", userSchema);
-
 //pre hook - we will be making sure that if someone is able to hack our database 
 //user passwords are not understable by the hacker . we will encrypt them before saving it in db.
 //this is what we meant by pre hook - basically a function that runs before saving the user passowrds to the database
 
 
 userSchema.pre("save", async function(next) {
+    if (!this.isModified("password")) return next(); 
     try{
         const salt = await bcrypt.genSalt(10);
         this.password = await bcrypt.hash(this.password, salt);
         next(); 
     }catch (error) {
-        next(error);
+        next(error);    
     }
 })
+
+const User = mongoose.model("User", userSchema);
+
+
 export default User;
