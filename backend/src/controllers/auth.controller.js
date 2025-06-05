082a74fa108a1d9826779487861b2aa8dec537ad
shaurya super <shauryabansal2005@gmail.com>
2025-06-05 10:53:35 +0530

    feat: add onboarding routes, update Stream user data, and protect profile access

--- backend/src/controllers/auth.controller.js
+++ backend/src/controllers/auth.controller.js
@@ -107,3 +107,49 @@ export function logout(req, res) {
     res.clearCookie("jwt");
     res.status(200).json({succeess: true , message: "Logged out successfully"});
 }
+
+export async function onboard(req, res) {
+    try {
+        const userId = req.user._id;
+        const {fullName, bio, nativeLanguage, learningLanguage, location} = req.body;
+
+        if(!fullName || !bio || !nativeLanguage || !learningLanguage || !location) {
+            return res.status(400).json({
+                message: "All fields are required",
+                missingFields: [
+                    !fullName && "fullName",
+                    !bio && "bio",
+                    !nativeLanguage && "nativeLanguage",
+                    !learningLanguage && "learningLanguage",
+                    !location && "location"
+                ],
+            });
+        }
+
+        const updatedUser = await User.findByIdAndUpdate(
+            userId,
+            { ...req.body, isOnBoarded: true },
+            { new: true }
+        );
+
+        if(!updatedUser) {
+            return res.status(404).json({message: "User not found"});
+        }
+        try {
+            upsertStreamUser({
+                id: updatedUser._id.toString(),
+                name: updatedUser.fullName,
+                image: updatedUser.profilePic || ""
+            });
+            console.log(`Stream user updated for onboarded user: ${updatedUser.fullName}`);
+        } catch (streamError) {
+            console.error("Error updating stream user:", streamError.message);
+        }
+
+
+        res.status(200).json({success: true, user: updatedUser});
+    } catch (error) {
+        console.error("Onboarding Error", error);
+        res.status(500).json({message: "Internal server error"});
+    }
+}
--- backend/src/server.js
+++ backend/src/server.js
@@ -2,11 +2,13 @@ import express from 'express';
 import "dotenv/config"
 import authRoutes from './routes/auth.route.js';
 import { connectDB } from './lib/db.js';
+import CookieParser from 'cookie-parser';
 
 const app = express();
 const PORT = process.env.PORT;
 
 app.use(express.json());
+app.use(CookieParser());
 app.use("/api/auth", authRoutes)
 
 app.listen(PORT , () => {
