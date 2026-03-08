b838a6a3bc220ff64b986cd854d2daf09da586d6
shaurya super <shauryabansal2005@gmail.com>
2026-03-08 16:57:18 +0530

    made some inportant changes

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,4 +1,4 @@
-import { Routes, Route } from 'react-router-dom'
+import { Routes, Route , Navigate } from 'react-router-dom'
 import { Toaster } from 'react-hot-toast'
 import HomePage from './pages/HomePage.jsx'
 import SignUpPage from './pages/SignUpPage.jsx'
@@ -12,27 +12,27 @@ import axios from 'axios'
 import { axiosInstance } from './lib/axios.js'
 
 const App = () => {
-  const { data, isLoading, error } = useQuery({
-    queryKey: ['todos'],
+  const { data:authData, isLoading, error } = useQuery({
+    queryKey: ['authUser'],
     queryFn: async () => {
-      const res = await axiosInstance.get("http://localhost:5001/api/auth/me")
+      const res = await axiosInstance.get("/auth/me")
       return res.data
     }
   })
 
-  console.log(data)
+  const authUser = authData?.user
 
 
   return (
-    <div className='h-screen text-5xl' data-theme="night">
+    <div className='h-screen' data-theme="night">
       <Routes>
-        <Route path="/" element={<HomePage />} />
-        <Route path="/signup" element={<SignUpPage />} />
-        <Route path="/login" element={<LoginPage />} />
-        <Route path="/notifications" element={<NotificationsPage />} />
-        <Route path="/call" element={<CallPage />} />
-        <Route path="/chat" element={<ChatPage />} />
-        <Route path="/onboarding" element={<OnboardingPage />} />
+        <Route path="/" element={authUser ? <HomePage /> : <Navigate to="/login" />} />
+        <Route path="/signup" element={!authUser ? <SignUpPage /> : <Navigate to="/" />} />
+        <Route path="/login" element={!authUser ? <LoginPage /> : <Navigate to="/" />} />
+        <Route path="/notifications" element={authUser ? <NotificationsPage /> : <Navigate to="/login" />} />
+        <Route path="/call" element={authUser ?<CallPage /> : <Navigate to="/login" /> } />
+        <Route path="/chat" element={authUser ?<ChatPage /> : <Navigate to="/login" /> } />
+        <Route path="/onboarding" element={authUser ? <OnboardingPage /> : <Navigate to="/login" />} />
       </Routes>
 
       <Toaster />
--- frontend/src/lib/axios.js
+++ frontend/src/lib/axios.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
 export const axiosInstance = axios.create({
-    baseURL: "https://lo0calhost:5001/api",
+    baseURL: "http://localhost:5001/api",                              
     withCredentials: true,
 })
